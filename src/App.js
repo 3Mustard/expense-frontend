949fd9f31aceed660cc8bc28ace57e07915ea4c7
3Mustard <m23.cullen@gmail.com>
2020-02-06 20:26:40 -0500

    change local port of npm start to 3001

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
+const FETCH_URL = 'http://localhost:3000/api/v1/accounts'
 
 class App extends Component {
+
+    componentDidMount() {
+        fetch(FETCH_URL)
+        .then(response => response.json())
+        .then(jsonResponse => console.log(jsonResponse)); 
+    }
+
     render() {
         return (
             <div className="App">
