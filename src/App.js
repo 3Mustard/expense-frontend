40de98c27e1d08fff8033596e7db23507e90612e
3Mustard <m23.cullen@gmail.com>
2020-02-06 21:11:59 -0500

    add redux dependencies and import into index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
-const FETCH_URL = 'http://localhost:3000/api/v1/accounts'
+const ACCOUNTS_FETCH_URL = 'http://localhost:3000/api/v1/accounts'
 
 class App extends Component {
 
     componentDidMount() {
-        fetch(FETCH_URL)
+        fetch(ACCOUNTS_FETCH_URL, {
+            method: 'GET'
+        })
         .then(response => response.json())
-        .then(jsonResponse => console.log(jsonResponse)); 
+        .then(jsonResponse => console.log(jsonResponse));
     }
 
     render() {
